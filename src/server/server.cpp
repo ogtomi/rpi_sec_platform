b4e8a6c34b641ee9da4083c9a90abf25b634d9aa
Tomasz Lewandowski <tomeklewandowski570@gmail.com>
2023-12-04 14:44:52 +0100

    Server: Added basic server to test socket communication

---
+++ src/server/server.cpp
@@ -0,0 +1,46 @@
+#include "server.hpp"
+
+Server::Server(int domain, int service, int protocol, int port, u_long interface, int bcklg)
+{
+    socket = new ListeningSocket(domain, service, protocol, port, interface, bcklg);
+}
+
+ListeningSocket* Server::get_socket()
+{
+    return socket;
+}
+
+void Server::accepter()
+{
+    struct sockaddr_in address = socket->get_address();
+    int addrlen = sizeof(address);
+
+    new_socket = accept(socket->get_sock(), (struct sockaddr*) &address, (socklen_t*) &addrlen);
+
+    read(new_socket, buffer, BUFF_SIZE);
+}
+
+void Server::handler()
+{
+    std::cout << buffer << std::endl;
+}
+
+void Server::responder()
+{
+    char* hello = "Hello from the server";
+
+    write(new_socket, hello, std::strlen(hello));
+    close(new_socket);
+}
+
+void Server::launch()
+{
+    while(true)
+    {
+        std::cout << "======= WAITING =======" << std::endl;
+        accepter();
+        handler();
+        responder();
+        std::cout << "======= DONE =======" << std::endl;
+    }
+}
---
+++ src/server/server.hpp
@@ -0,0 +1,27 @@
+#ifndef _SERVER_H_
+#define _SERVER_H_
+
+#include <unistd.h>
+#include <cstring>
+#include "../sockets/listening_socket.hpp"
+
+class Server
+{
+private:
+    enum { BUFF_SIZE = 30000};
+    char buffer[BUFF_SIZE] = {0};
+    int new_socket;
+    ListeningSocket* socket;
+
+public:
+    Server(int domain, int service, int protocol, int port, u_long interface, int bcklg);
+    void launch();
+    ListeningSocket* get_socket();
+
+private:
+    void accepter();
+    void handler();
+    void responder();
+};
+
+#endif
